b32c74f3dd98b171b38021610a5d764779a4e719
tanjiroyt235 <tanjiroyt235@gmail.com>
2025-05-14 10:23:13 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+function keepAlive() {
+  app.listen(3000, () => {
+    console.log('🌐 Web server running on port 3000');
+  });
+}
+
+module.exports = keepAlive; // Export the keep-alive function
 
